26cececae52662f9f20cda0bde8f337b29f59d95
Hanss <hanifalaslam098@gmail.com>
2024-03-15 00:56:49 +0700

    Update
    Update

--- src/login.js
+++ src/login.js
@@ -1,9 +1,107 @@
-function Login(){
-    return(
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    )
-}
+import React from "react";
+import "./App.css";
+import styles from "./menu.css"; 
+const login = (props) => {
+  return (
+    <div className="c_frame21-frame">
+      <div className="c_frame21-frame01">
+        <div className="c_frame21-frame02">
+          <div className="c_frame21-frame03">
+            <div className="c_frame21-frame04">
+              <div className="c_frame21-text">
+                <p className="c_frame21-text01">Login</p>
+              </div>
+            </div>
+            <div className="c_frame21-text02">
+              <p className="c_frame21-text03">Login to your Swap Account !</p>
+            </div>
+          </div>
+          <img
+            src="./swap.png"
+            alt="vector"
+            width={110.5}
+            height={100.3638916015625}
+            className="c_frame21-vector"
+            />
+          <div className="c_frame21-frame05">
+            <div className="c_frame21-text04">
+              <p className="c_frame21-text05">Email/Username</p>
+            </div>
+            <div className="c_frame21-frame06">
+              <div className="c_frame21-text06">
+                <p className="c_frame21-text07">Your Email Address/Username</p>
+              </div>
+            </div>
+          </div>
+          <div className="c_frame21-frame07">
+            <div className="c_frame21-text08">
+              <p className="c_frame21-text09">Password</p>
+            </div>
+            <div className="c_frame21-frame08">
+              <div className="c_frame21-text10">
+                <p className="c_frame21-text11">Your Password</p>
+              </div>
+            </div>
+          </div>
+          <div className="c_frame21-frame09">
+            <div className="c_frame21-text12">
+              <p className="c_frame21-text13">Login</p>
+            </div>
+          </div>
+          <div className="c_frame21-text14">
+            <p className="c_frame21-text15">
+              <span className="c_frame21-text16">Forgot your password?</span>
+                <span className="c_frame21-text17">Get Help</span>
+                </p>
+                </div>
+                <img
+                  src="./eco.png"
+                  alt="frame"
+                  width={558}
+                  height={3}
+                  className="c_frame21-frame10"
+                  />
+                <div className="c_frame21-frame11">
+                  <img
+                    src="./google.png"
+                    alt="instance"
+                    width={24}
+                    height={24.101776123046875}
+                    className="c_frame21-instance"
+                    />
+                  <div className="c_frame21-text18">
+                    <p className="c_frame21-text19">Continue with Google</p>
+                  </div>
+                </div>
+                <div className="c_frame21-frame12">
+                  <img
+                    src="./facebook.png"
+                    alt="instance"
+                    width={24}
+                    height={24}
+                    className="c_frame21-instance1"
+                    />
+                  <div className="c_frame21-text20">
+                    <p className="c_frame21-text21">Continue with Facebook</p>
+                  </div>
+                </div>
+                <div className="c_frame21-text22">
+                  <p className="c_frame21-text23">
+                    <span className="c_frame21-text24">Doesn’t have an account?</span>
+                      <span className="c_frame21-text25">Register Now</span>
+                      </p>
+                      </div>
+                      </div>
+                      </div>
+                      <img
+                        src="./eco.png"
+                        alt="frame"
+                        width={712}
+                        height={1080}
+                        className="c_frame21-frame13"
+                        />
+                      </div>
+                      )
+                      }
 
-export default Login
+export default login;
